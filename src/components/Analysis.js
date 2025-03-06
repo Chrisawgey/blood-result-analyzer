7a19d96a328410398c5db428d6159485b1a89064
chrisawgey <chrisvpopoca@gmail.com>
2025-03-06 13:02:56 -0500

    quick chnage

--- src/components/Analysis.js
+++ src/components/Analysis.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
+
 function Analysis() {
   const [ocrText, setOcrText] = useState('');
   const [extractedData, setExtractedData] = useState({});
